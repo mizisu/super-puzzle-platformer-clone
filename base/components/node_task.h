c0a4406a838a4be8caad45197a9ed77791775bc9
mizisu <alwltn960406@gmail.com>
2019-05-02 17:02:00 +0900

    impl node task

---
+++ base/components/node_task.h
@@ -0,0 +1,31 @@
+#ifndef __NODE_TASK_H__
+#define __NODE_TASK_H__
+
+#include "base/components/node.h"
+
+class NodeTask : public Node {
+ public:
+  static auto Interval(std::function<void()> function, double interval) {
+    auto task = std::make_shared<NodeTask>();
+    task->function = function;
+    task->interval = interval;
+    return task;
+  }
+
+ public:
+  NodeTask() : function(nullptr), interval(0) {}
+  virtual ~NodeTask() { this->function = nullptr; }
+  virtual void Update() override {
+    this->interval -= Global::DeltaTime;
+    if (this->interval < 0 && this->function != nullptr) {
+      this->function();
+      this->Erase();
+    }
+  }
+
+ private:
+  std::function<void()> function;
+  double interval;
+};
+
+#endif  // __NODE_TASK_H__
